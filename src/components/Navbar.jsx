5b0a0c6dffe3bed1c2110b62f5bae41d25eee5c1
jayPreak <jayesh.preak2003@gmail.com>
2023-03-13 21:05:03 +0530

    heavy stuff

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../style";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
+const Navbar = () => {
+  const [active, setActive] = useState("");
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Jayesh{" "}
+            <span className="sm:block hidden">| Full-Stack Developer</span>
+          </p>
+        </Link>
+        <p className="text-red-500">yep</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
