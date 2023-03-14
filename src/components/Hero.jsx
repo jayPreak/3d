bb0437b416083bdca9288897f48c6571e9b52801
jayPreak <jayesh.preak2003@gmail.com>
2023-03-14 20:57:56 +0530

    aesthetics :nerd:

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,8 +10,22 @@ const Hero = () => {
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-5 h-5 rounded-full bg-[#b71758]" />
+          <div className="w-1 sm:h-80 h-40 bg-gradient-to-b from-pink-700 " />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi I'm <span className="text-[#b71758]">Jayesh</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            Passionate about crafting seamless digital
+            <br className="sm:block hidden" /> experiences that bridge the gap
+            between
+            <br className="sm:block hidden" />
+            <span className="text-[#b71758]">aesthetics</span> and{" "}
+            <span className="text-[#b71758]">functionality</span>
+          </p>
         </div>
       </div>
     </section>
