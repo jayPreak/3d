86daee515c518d5cc6969603933b9d622d5367f4
jayPreak <jayesh.preak2003@gmail.com>
2023-03-14 17:32:53 +0530

    hamborger fuschia and r0see? 😮

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,11 +42,30 @@ const Navbar = () => {
         </ul>
         <div className="sm:hidden flex flex-1 justify-end items-center">
           <img
-            src={menu}
+            src={toggle ? close : menu}
             alt="menu"
             className="w-[28px] h-[28px] object-contain cursor-pointer"
             onClick={() => setToggle(!toggle)}
           />
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 bg-gradient-to-r from-rose-900 to-pink-700 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } hover:text-white text-[18px] font-medium cursor-pointer`}
+                  onClick={() => setActive(link.title)}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
