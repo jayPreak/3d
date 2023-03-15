8d1bc955c1f64ff2d44bafb2edbe7f4f80321c0c
jayPreak <jayesh.preak2003@gmail.com>
2023-03-15 21:10:08 +0530

    watevr

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,14 +4,27 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
-  const computer = useGLTF("./desktop_pc/kindred.gltf");
+const Computer = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
@@ -30,10 +43,10 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computer />
       </Suspense>
       <Preload all />
     </Canvas>
   );
 };
-export default Computers;
+export default ComputersCanvas;
