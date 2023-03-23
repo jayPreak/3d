b2c2482c92c4b9e1c64b0a53f3e0010a3780dcd7
jayPreak <jayesh.preak2003@gmail.com>
2023-03-23 23:06:06 +0530

    ballz XD

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flex flex-row flex-wrap justify-center gap-0">
+      {technologies.map((tech) => (
+        <div className="w-28 h-28" key={tech.name}>
+          <BallCanvas icon={tech.icon} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default Tech;
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Ball = () => {
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
   return (
-    <div>Ball</div>
-  )
-}
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        {/* <meshLambertMaterial color="white" /> */}
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          map={decal}
+        />
+      </mesh>
+    </Float>
+  );
+};
 
-export default Ball
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop="demand"
+      // shadows
+      // camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          // autoRotate={true}
+          // enableDamping={true}
+          enableZoom={false}
+          // enablePan={true}
+          // enableRotate={true}
+          // rotation={[10, -5, 20]}
+          // attach={true}
+          // minAzimuthAngle={-Math.PI}
+          // maxAzimuthAngle={Math.PI / 2}
+          // minPolarAngle={Math.PI / 2}
+          // maxPolarAngle={Math.PI / 2}
+        />
+        <Ball imgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default BallCanvas;
