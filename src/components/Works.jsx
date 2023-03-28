91adadb4999bc533331e1653fc0783bd50d394c1
jayPreak <jayesh.preak2003@gmail.com>
2023-03-28 21:30:22 +0530

    yummy juicy yippee subway coming >.<

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -15,6 +15,15 @@ const Works = () => {
         <p className={styles.sectionSubText}>What I have done so far</p>
         <h2 className={styles.sectionHeadText}>Work Experience.</h2>
       </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Yum
+        </motion.p>
+      </div>
     </>
   );
 };
