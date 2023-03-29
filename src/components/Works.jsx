933dd773547015f80da00035f5f525004e239d0c
jayPreak <jayesh.preak2003@gmail.com>
2023-03-29 17:32:26 +0530

    .nerdge

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,6 +7,29 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ProjectCard = ({
+  index,
+  title,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      {/* test */}
+      <Tilt
+        options={{ max: 45, scale: 1, speed: 450 }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img src={image} alt={name} />
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
+
 const Works = () => {
   return (
     // <div>Works</div>
@@ -21,8 +44,18 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          Yum
+          As someone who is passionate about creating visually stunning and
+          functional websites, I've worked on a diverse range of projects over
+          the years. In this section, I'm excited to share some of my favorite
+          projects that highlight my skills in full-stack web development,
+          UI/UX, and graphic design.
         </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
       </div>
     </>
   );
