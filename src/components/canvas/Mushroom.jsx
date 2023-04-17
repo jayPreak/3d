cb3011f816ba97e48c046637fdf180cb9d5db2ab
jayPreak <jayesh.preak2003@gmail.com>
2023-04-17 18:48:49 +0530

    responsive mooshrooowom

--- src/components/canvas/Mushroom.jsx
+++ src/components/canvas/Mushroom.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Suspense } from "react";
+import { useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Mushroom = () => {
+const Mushroom = ({ isMobile }) => {
   const shroom = useGLTF("./stylized_mushrooms/scene.gltf");
   return (
     <primitive
       object={shroom.scene}
-      scale={0.85}
+      scale={isMobile ? 1.2 : 0.85}
       position-y={-1}
       // rotation-y={0}
       rotation={[0.1, 0, 0]}
@@ -19,6 +20,19 @@ const Mushroom = () => {
 };
 
 const MushroomCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width:500px)");
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
   return (
     <Canvas
       shadows
@@ -39,7 +53,7 @@ const MushroomCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Mushroom />
+        <Mushroom isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
